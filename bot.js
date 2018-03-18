5f1ffffc0fd251bf1387c9a4c70b9a537386a248
Saar Ovadia <36736277+skylinefaceit@users.noreply.github.com>
2018-03-18 18:30:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,26 +17,37 @@ bot.on('ready', () => {
 	console.log(`Bot is up and running, with ${ util.discord.getOnlineUsers(bot.users) } online users, in ${ bot.channels.size } channels.`);
 });
 
-// Message is emitted whenever a message on the server is sent.
-bot.on('message', async message => {
-  // No point dealing with the message if it was sent by a bot!
-	if (message.author.bot) return;
 
-  // If the message doesn't contain the command prefix, we might as well leave it alone.
-	if (message.content.indexOf(config.prefix) !== 0) return;
 
-  // Get the the command if it was the first word that was sent.
-	const command = util.discord.getCommand(message);
 
-  // If the command that was sent matches any of the commands that we have configured...
-	if (command === 'ping') {
-    // It's nice to have a log of when the command was sent for future reference along with the person that sent it.
-		console.log(`Command [${ config.prefix + command }] Received from ${ message.author.username } at ${ util.helpers.convertTime(message.createdTimestamp) }`);
 
-    // Send a message to the channel where the initial message came from.
-    const m = await message.channel.send('Pong!');
+var request = require('request');
+
+var playerName = "robi62"; // player in game name
+
+var options = {
+  method: "GET",
+  url: `https://fortnite.y3n.co/v2/player/${playerName}`,
+  headers: {
+    'User-Agent': 'nodejs request',
+    'X-Key': "API KEY HERE"
   }
-});
+}
+
+request(options, (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    var stats = JSON.parse(body);
+    console.log(`robi62 k/d: ${stats.br.stats.pc.all.kpd}`);
+  }
+})
+    
+
+
+
+
+
+
+
 
 // This logs the bot into the server itself.
 bot.login(process.env.logintoken);
